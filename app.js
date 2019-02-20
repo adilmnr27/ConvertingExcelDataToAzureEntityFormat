83f85ed8b0416768b5d9426e91aec7ff757c5e2d
Mansoor <adil.mansoor@capgemini.com>
2019-02-20 17:32:06 +0530

    some comments added for clearity

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const XLSX = require('xlsx');
 const { Schema } = require('./schemaClass.js');
 const fs = require('fs');
 
+//reading excel file
 const workbook = XLSX.readFile('./List.xlsx');
 const sheetNameList = workbook.SheetNames;
+
+//storing all the values from excel sheet first column into values variable in JSON format
 var values = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]);
 console.log(values);
 var list = [];
@@ -18,6 +21,5 @@ fs.writeFile("./result.JSON", list, function (err) {
     if (err) {
         return console.log(err);
     }
-
     console.log("The file was saved!");
 });
